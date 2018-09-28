6c4b60b5a267e73737cb1d3555ddc975ac8dc7af
Iván Sánchez Ortega <ivan@sanchezortega.es>
2018-09-28 10:23:44 +0200

    rm sander module, use custom promise-based filesystem functionality

--- parsers/Course.js
+++ parsers/Course.js
@@ -1,7 +1,7 @@
 /* eslint no-underscore-dangle: "off" */
 
-import { readFile } from 'sander';
 import path from 'path';
+import { readFile } from './fs-promises';
 import Recipe from './Recipe';
 import appliesToRunningPlatform from './platform-check';
 
---
+++ parsers/fs-promises.js
@@ -0,0 +1,35 @@
+// Ideally, these promise-based filesystem operations should be handled by
+// some module like 'sander' or 'fs-extra'. Unfortunately using an external
+// module causes problems when running the build application outside of
+// a development environment.
+//
+// Please note that such modules handle some edge cases and race conditions,
+// and this naïve implementation doesn't.
+
+
+import fs from 'fs';
+
+export function readFile(filename, options) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, options, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+export function stat(filename) {
+    return new Promise((resolve, reject) => {
+        fs.stat(filename, (err, stats) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(stats);
+            }
+        });
+    });
+}
+
