0949a796722387f2e94b3ed54e224655c2a77021
aadnekar <aadne.karstad@gmail.com>
2022-06-02 12:09:38 +0200

    Remove SidePanel and Course Steps
    Remove unused functionality to emphasize deprecation

--- lib/components/CourseView.jsx
+++ lib/components/CourseView.jsx
@@ -28,109 +28,22 @@ const recipeState = recipeCheckables => {
     return 'in-progress';
 };
 
-const btnClassName = 'checkable-button btn btn-primary btn-nordic';
-const deprecated = true;
-
-const DeprecationWarning = () =>
-    deprecated ? (
-        <Alert variant="warning" className="description-view mb-3">
-            <Alert.Heading className="mb-3">Deprecation warning</Alert.Heading>
-            <p>
-                This app has been replaced by the Toolchain Manager app. Go to
-                the app overview to install and open the{' '}
-                <b>Toolchain Manager</b>.
-            </p>
-            <p className="mb-0">
-                The Toolchain Manager is available for Windws, Mac and Ubuntu operating
-                systems and installs the full toolchain that you need to work
-                with the nRF Connect SDK.
-            </p>
-        </Alert>
-    ) : null;
-
+const DeprecationWarning = () => (
+    <Alert variant="warning" className="description-view mb-3">
+        <Alert.Heading className="mb-3">Deprecation warning</Alert.Heading>
+        <p>
+            This app has been replaced by the Toolchain Manager app. Go to the
+            app overview to install and open the <b>Toolchain Manager</b>.
+        </p>
+        <p className="mb-0">
+            The Toolchain Manager is available for Windws, Mac and Ubuntu
+            operating systems and installs the full toolchain that you need to
+            work with the nRF Connect SDK.
+        </p>
+    </Alert>
+);
 const CourseView = ({ description, recipes, checkables, checkAll }) => (
-    <div className="course-view">
-        <Tab.Container id="course-view" defaultActiveKey={0} transition={false}>
-            <Row className="clearfix">
-                <Col sm={3}>
-                    <Nav variant="pills" className="flex-column" as="ul">
-                        <Nav.Item as="li">
-                            <Nav.Link eventKey={0}>
-                                <i className="recipe-number">
-                                    <span className="mdi mdi-star" />
-                                </i>
-                                <span className="recipe-title">Overview</span>
-                            </Nav.Link>
-                        </Nav.Item>
-                        {!deprecated &&
-                            recipes.map(({ title, tool }, index) => (
-                                <Nav.Item
-                                    as="li"
-                                    key={`${index + 1}`}
-                                    className={`${recipeState(
-                                        checkables[tool]
-                                    )}`}
-                                >
-                                    <Nav.Link eventKey={index + 1}>
-                                        <i className="recipe-number">
-                                            {index + 1}
-                                        </i>
-                                        <span className="recipe-title">
-                                            {title}
-                                        </span>
-                                    </Nav.Link>
-                                </Nav.Item>
-                            ))}
-                    </Nav>
-                </Col>
-                <Col sm={9}>
-                    <Tab.Content>
-                        <Tab.Pane eventKey={0} className="course-pane">
-                            {deprecated ? (
-                                <DeprecationWarning />
-                            ) : (
-                                <>
-                                    <h3>Overview</h3>
-                                    <DescriptionView
-                                        description={description}
-                                    />
-                                </>
-                            )}
-                        </Tab.Pane>
-                        {!deprecated &&
-                            recipes.map((recipe, index) => (
-                                <Tab.Pane
-                                    eventKey={index + 1}
-                                    key={`${index + 1}`}
-                                    className={`recipe-pane ${recipeState(
-                                        checkables[recipe.tool]
-                                    )}`}
-                                >
-                                    <div className="course-title">
-                                        <h3>{recipe.title}</h3>
-                                        {recipe.checkables.find(
-                                            ({ checkers }) => !!checkers
-                                        ) !== undefined && (
-                                            <ButtonGroup className="checkable-button-group">
-                                                <Button
-                                                    className={btnClassName}
-                                                    onClick={() =>
-                                                        checkAll(recipe)
-                                                    }
-                                                >
-                                                    Verify all
-                                                </Button>
-                                            </ButtonGroup>
-                                        )}
-                                    </div>
-                                    <RecipeView {...recipe} />
-                                </Tab.Pane>
-                            ))}
-                    </Tab.Content>
-                </Col>
-            </Row>
-        </Tab.Container>
-    </div>
+    <DeprecationWarning />
 );
 
 CourseView.propTypes = {
